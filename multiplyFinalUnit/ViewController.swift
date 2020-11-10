0f5eff7b837d6a13b0f454992c75489ee4d2e7f2
romanthegg <rzadorskyy3454@stu.d214.org>
2020-11-10 16:36:26 -0500

    added multiply function fully working. when button is clicked, the product of two numbers will come up

--- multiplyFinalUnit/ViewController.swift
+++ multiplyFinalUnit/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var number1: UITextField!
+    
+    @IBOutlet weak var number2: UITextField!
+    
+    @IBOutlet weak var product: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //MVP SECTION
+        view.backgroundColor = .cyan
     }
 
+    @IBAction func button(_ sender: Any) {
+        let data1 = number1.text!
+        let data2 = number2.text!
+        
+        let number1 = Int(data1)!
+        let number2 = Int(data2)!
+        
+        let answer = number1 * number2
+        
+        product.text = String(answer)
+    }
+  
+    }
+    
 
-}
 
